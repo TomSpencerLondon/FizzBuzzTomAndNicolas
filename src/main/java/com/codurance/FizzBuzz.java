ad1ccabb6e5e8a78d3400a3deb3a9aeb147166ba
nicolas belfis <nicolasbelfis@nicolass-MacBook-Pro.local>
2020-09-07 12:32:27 +0200

    fizzbuzz first tests - green

---
+++ src/main/java/com/codurance/FizzBuzz.java
@@ -0,0 +1,10 @@
+package com.codurance;
+
+public class FizzBuzz {
+
+    public String convert(int number) {
+        if(0 == number) return "";
+        else return String.valueOf(number);
+
+    }
+}
--- src/test/java/com/codurance/FizzBuzzShould.java
+++ src/test/java/com/codurance/FizzBuzzShould.java
@@ -1,5 +1,28 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzShould {
+
+    @Test
+    public void return_empty_string_when_number_is_zero() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String output = fizzBuzz.convert(0);
+        assertEquals("", output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,1","2,2"})
+    public void return_1_when_number_is_one(int input, String outputExpected) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualOutput = fizzBuzz.convert(input);
+        assertEquals(outputExpected, actualOutput);
+
+    }
+
 
 }
